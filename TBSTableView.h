0df8532f91d17a429d1c6b3bdba5288d14c73395
Matt Austin <matt@tunabelly.com>
2017-03-02 10:06:05 -0500

    First checkin.

---
+++ TBSTableView.h
@@ -0,0 +1,15 @@
+//
+//  TBSTableView.h
+//
+//  Created by Matt Austin on 2017-03-02.
+//  Copyright (c) 2017 Tunabelly Software Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TBSTableView : NSTableView
+
+@property NSString *uniqueIdentifier;
+@property NSSortDescriptor *defaultSortDescriptor;
+
+@end
---
+++ TBSTableView.m
@@ -0,0 +1,87 @@
+//
+//  TBSTableView.m
+//
+//  Created by Matt Austin on 2017-03-02.
+//  Copyright (c) 2017 Tunabelly Software Inc. All rights reserved.
+//
+
+#import "TBSTableView.h"
+
+@interface TBSTableView ()
+
+- (void)loadAndSetSortDescriptors;
+- (void)saveSortDescriptors;
+
+@end
+
+@implementation TBSTableView
+
+- (void)keyDown:(NSEvent *)theEvent
+{
+	// handle delete
+	unichar key = [[theEvent charactersIgnoringModifiers] characterAtIndex:0];
+	if (key == NSDeleteCharacter && self.selectedRow != -1) {
+		[NSApp sendAction:@selector(delete:) to:nil from:self];
+	}
+	else {
+		[super keyDown:theEvent];
+	}
+}
+
+- (void)viewWillMoveToWindow:(nullable NSWindow *)newWindow
+{
+	[super viewWillMoveToWindow:newWindow];
+
+	// check
+	NSAssert(self.uniqueIdentifier!=nil, @"uniqueIdentifier cannot be nil - please fill in before the table is shown.");
+	
+	if (newWindow != nil)
+	{
+		// window it about to be shown
+		[self loadAndSetSortDescriptors];
+	}
+	else
+	{
+		// window is closing
+		[self saveSortDescriptors];
+	}
+}
+
+#pragma mark - Loading/Saving Sort Descriptors
+
+- (void)loadAndSetSortDescriptors
+{
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	
+	// check if there's a stored sort descriptors
+	NSArray *sortDescriptors = nil;
+	NSData *storedData = [userDefaults objectForKey:self.uniqueIdentifier];
+	if (storedData != nil)
+	{
+		// load the stored sort descriptors
+		sortDescriptors = [NSUnarchiver unarchiveObjectWithData:storedData];
+	}
+	else
+	{
+		// use the default one
+		if (self.defaultSortDescriptor != nil) {
+			sortDescriptors = @[self.defaultSortDescriptor];
+		}
+	}
+
+	// set the sort descriptors
+	[self setSortDescriptors:sortDescriptors];
+}
+
+- (void)saveSortDescriptors
+{
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	
+	// save the table sort descriptors
+	NSData *storedData = [NSArchiver archivedDataWithRootObject:[self sortDescriptors]];
+	
+	[userDefaults setObject:storedData forKey:self.uniqueIdentifier];
+	[userDefaults synchronize];
+}
+
+@end
